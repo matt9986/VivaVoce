b14f784b8adbf286ccce26b04a2a712dec5f9e2d
Matt <Matt@smp-mobile.(none)>
2013-09-02 11:20:37 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VivaVoce::Application.config.secret_token = '130d17f0d3448b2f20c24c6eb982dae3f541ac0be793b7f78b1344d129773c0c38caa9404c0a62db2b4c662e0e1cb20b2a8328fb4b502ee62415e02de8bd4222'
