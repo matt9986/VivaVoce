72d41449bfa6f091d15bc6d924cab85674fafcd2
Matt <Matt@smp-mobile.(none)>
2013-09-07 20:51:45 -0700

    Added stars saving to create review method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,12 +5,15 @@ class ReviewsController < ApplicationController
 	def create
 		params[:review][:user_id] = current_user.id
 		params[:review][:business_id] = params[:business_id]
+		@business = Business.find(params[:business_id])
 		begin
 			ActiveRecord::Base.transaction do
 				@review = Review.new(params[:review])
 
 				@review.save
 				raise "invalid" unless @review.valid?
+				raise "invalid" unless (1 .. 5).include?(@params[:stars])
+				@business.add_evaluation(:stars, params[:stars], @review)
 			end
 		rescue
 			render json: @review.errors.full_messages, status: 422
@@ -39,5 +42,12 @@ class ReviewsController < ApplicationController
 		unless current_user.id == @review.user_id
 			render json: "You're not allowed", status: 403
 		end
+	end
+
+	def vote
+		value = params[:vote] == "down" ? -1 : 1
+		@review = Review.find(params[:id])
+		@review.add_or_update_evaluation(:votes, value, current_user)
+		render json: {vote: value}, status: 200
 	end
 end
